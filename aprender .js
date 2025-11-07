37e513c3a8a8dc998db78531c08b0466f976591b
CFOG <carlosfranciscoortegagonzalez@gmail.com>
2025-11-07 09:43:38 -0600

    mi primer clase de variables js

---
+++ aprender .js
@@ -0,0 +1,36 @@
+let edad = 17;  //variable de tipo numero int
+let nombre = "carlos";  //variable de tipo texto(string)
+let nombreDeLaEscuela = "uvm";
+let nombreAmigo = "juan";
+console.log(edad);
+console.log(nombre);  //muestra en consola del navegador, ayuda a detectar errores
+
+const pi = 3.14;
+const miNombre = "carlos";
+const nombreEscuela = "UVM";
+const gravedad = 9.81;  //variable que nunca cambia
+console.log(pi);
+console.log(miNombre);
+
+let nombreA = "carlos";
+let nombrez = "juan";
+let ejemplo = `Hola ${nombreA}`;
+let nombret = "hola carlos"
+let nombrew = `hola ${nombrez}`;   //aqui va a imprimir "hola juan"
+console.log(ejemplo);
+console.log(nombrew);
+
+let edadw = 17;
+let edadDeAmigo = 16;
+let edade = 18
+let edades = 19  //la variable tipo int solo sirve para numeros enteros
+console.log(edadw)
+
+let suma = 10 + 20;  //30
+let resta = 9 - 2;  //7
+let multiplicacion = 20 * 2;  //40
+let division = 40 / 4;  //10
+let modulo = 10 % 3;  //1
+let potencia = 2 ** 2;  //8
+
+
